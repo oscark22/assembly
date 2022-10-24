1d8bd466ea92784145634883071baa694b27d62a
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-10-24 16:04:21 -0700

    finished num of consonants

---
+++ modified_dot_product.asm
@@ -0,0 +1,56 @@
+section .data
+    ve1: dd 1,2,3
+    ve2: dd 2,2,1
+
+section .text
+    global _start
+
+_start:
+    ;Address direction
+    mov ebx,ve1 
+    mov edx,ve2
+    mov ecx,1
+    mov eax,0
+    mov [sum],eax ;init the result of the dot product
+loop:
+    mov [con],ecx
+    mov eax,[ebx] ;Element of vec1
+    mov ecx,[edx] ;Element of vec2
+    mul ecx
+    ; Add to the result
+    mov ebx,[sum]
+    add eax,ebx
+    mov [sum],eax
+    ; Move the address 4 units each cicle, i.e. 4,8,12,...
+    mov ecx,[con]
+    mov eax,4
+    mul ecx
+
+    mov ebx,ve1
+    add ebx,eax 
+    mov edx,ve2
+    add edx,eax
+
+    inc ecx
+    cmp ecx,3
+    jle loop
+
+    ;print the result
+    mov eax,[sum]
+    add eax,'0'
+    mov [sum],eax
+
+    mov eax,4
+    mov ebx,1
+    mov ecx,sum
+    mov edx,1
+    int 0x80
+
+    mov eax,1
+    mov ebx,0
+    int 0x80
+
+section .bss
+    sum resd 1
+    con resd 1
+    
---
+++ num_of_consonants.asm
@@ -0,0 +1,92 @@
+section .data
+    wrd: dd 'qqewq'
+    msg1: dd 'The number of consonants is the following', 10
+    len1: equ $-msg1
+    msg2: dd '0'
+
+section .text
+    global _start
+
+_start:
+    mov eax, 0
+    mov [count], eax
+
+    mov ebx, wrd
+    mov al, [ebx]
+
+    mov ecx, 5
+
+loop:
+    call check_consonant
+
+    ; check counter
+    dec ecx
+    cmp ecx, 0
+    jz continue
+
+    jmp loop
+
+check_consonant:
+    cmp al, 'A',
+    je mov_byte
+    cmp al, 'a'
+    je mov_byte
+
+    cmp al, 'E',
+    je mov_byte
+    cmp al, 'e'
+    je mov_byte
+
+    cmp al, 'I',
+    je mov_byte
+    cmp al, 'i'
+    je mov_byte
+
+    cmp al, 'O',
+    je mov_byte
+    cmp al, 'o'
+    je mov_byte
+
+    cmp al, 'U',
+    je mov_byte
+    cmp al, 'u'
+    je mov_byte
+
+    ; increment count by 1
+    mov eax, [count]
+    inc eax
+    mov [count], eax
+
+    call mov_byte
+    ret
+
+mov_byte:
+    add ebx, 1
+    mov al, [ebx]
+    ret
+
+continue:
+    mov eax, [count]
+    add eax, [msg2]
+    mov [msg2], eax
+
+    ; print res
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg1
+    mov edx, len1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg2
+    mov edx, 1
+    int 0x80
+
+    ; end
+    mov eax, 1
+    mov ebx, 1
+    int 0x80
+
+section .bss
+     count resb 1   
